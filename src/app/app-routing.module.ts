c568964713df88186f604741bf8cf396b5f28d63
Veronikruiz <veroruiz06071985@gmail.com>
2022-07-30 21:58:55 -0300

    mi primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'login', component: LoginComponent},
+  { path: 'nuevaexp', component: NewExperienciaComponent},
+  { path: 'editexp/:id', component: EditExperienciaComponent},
+  { path: 'editedu/:id', component: EditeducacionComponent},
+  { path: 'nuevaedu', component: NeweducacionComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
